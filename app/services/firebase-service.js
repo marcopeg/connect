b3a5ddaf5fb24e2f6082f7640b157a511c6ee364
Marco Pegoraro <marco.pegoraro@gmail.com>
2015-09-26 17:27:24 +0200

    tick diff

--- app/services/firebase-service.js
+++ app/services/firebase-service.js
@@ -11,6 +11,8 @@ var fbDiscovery;
 var profileRef;
 var profileId;
 
+var tickValue;
+
 
 export function initFirebase() {
     return dispatch => {
@@ -40,9 +42,8 @@ export function initFirebase() {
         var tickRef = fb.child('tick');
 
         // start tick
-        setInterval($=> {
-            tickRef.transaction(val => (val || 0) + 1);
-        }, 500);
+        setInterval($=> tickRef.transaction(val => (val || 0) + 1), 500);
+        tickRef.on('value', snap => tickValue = snap.val());
 
     }
 }
@@ -61,26 +62,41 @@ export function startConnect() {
         dispatch(resetProfileList());
 
         // here there is subscriptions to be cleaned away!
-        generateDiscoveryId().then(discoveryId => {
-            var discoveryRef = fbDiscovery.child(discoveryId);
-            discoveryRef.child('profiles/' + profileId).set(Date.now());
+        // generateDiscoveryId().then(discoveryId => {
+        //     var discoveryRef = fbDiscovery.child(discoveryId);
+        //     discoveryRef.child('profiles/' + profileId).set(Date.now());
 
-            discoveryRef.child('profiles').on('child_added', snap => {
-                if (snap.key() !== profileId) {
-                    fbProfiles.child(snap.key()).once('value', snap => {
-                        dispatch(addProfile(snap.key(), snap.val()));
-                    });
-                }
-            });
-        });
+        //     discoveryRef.child('profiles').on('child_added', snap => {
+        //         if (snap.key() !== profileId) {
+        //             fbProfiles.child(snap.key()).once('value', snap => {
+        //                 dispatch(addProfile(snap.key(), snap.val()));
+        //             });
+        //         }
+        //     });
+        // });
 
-        // detect no results
-        setTimeout($=> {
-            if (getState().connect.profiles.length === 0) {
-                dispatch(setStep('nores'));
-                setTimeout($=> dispatch(changePage('start')), 1500);
+        // // detect no results
+        // setTimeout($=> {
+        //     if (getState().connect.profiles.length === 0) {
+        //         dispatch(setStep('nores'));
+        //         setTimeout($=> dispatch(changePage('start')), 1500);
+        //     }
+        // }, 10000);
+    
+
+        // expose myself for a while
+        var localTick = tickValue;
+        var discoveryRef = fbDiscovery.child(profileId);
+        discoveryRef.set(tickValue);
+        setTimeout($=> discoveryRef.remove(), 5000);
+
+        // search
+        fbDiscovery.on('child_added', snap => {
+            if (snap.key() !== profileId) {
+                var diff = Math.abs(localTick - snap.val());
+                console.log(snap.key(), diff);
             }
-        }, 10000);
+        });
 
     };
 }
