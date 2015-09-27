cb4aedeaa5d1e36a60f09d4bda274d7709267ac7
Marco Pegoraro <marco.pegoraro@gmail.com>
2015-09-27 07:46:53 +0200

    home disclaimer

--- app/containers/StartPage.js
+++ app/containers/StartPage.js
@@ -5,6 +5,7 @@ import { connect } from 'react-redux';
 import Grid from 'react-bootstrap/lib/Grid';
 import Well from 'react-bootstrap/lib/Well';
 import Glyphicon from 'react-bootstrap/lib/Glyphicon';
+import Label from 'react-bootstrap/lib/Label';
 
 import { StartScreen } from 'components/StartScreen';
 import { changePage } from 'services/active-page-service';
@@ -29,8 +30,8 @@ export class StartPage extends React.Component {
 
                 <p className="lead">
                     <b>ConnectAPP</b> allows you to quickly bond with other guys
-                    in <b>events as conferences</b> or such where there is no
-                    time to save a phone number or search through Facebook.
+                    in <b>events such <i>JSConf15</i></b> or when there is no
+                    time to save a phone numbers or search through Facebook.
                 </p>
                 <Well bsSize="xsmall" className="text-center">
                     <p className="lead" style={{margin:0}}>
@@ -41,6 +42,24 @@ export class StartPage extends React.Component {
                         <br /> to discover people around you. 
                     </p>
                 </Well>
+                <hr />
+                <div className="text-center">
+                    <p><b>!!! Attention !!!</b></p>
+                    <p className="lead">
+                        <i>
+                            This app was hacked in few hours after party the whole night
+                            at <b>JSConf15</b>.
+                        </i>
+                        <br /><br />
+                        Please <b>forgive the author</b> 
+                        if it doesn't work properly: <Label bsStyle="warning">It works on my machine!</Label>
+                    </p>
+                    <p className="lead">
+                        <a href="http://twitter.com/thepeg" target="_blank">@thepeg</a>
+                        <span> - </span>
+                        <a href="http://twitter.com/fiolkaf" target="_blank">@fiolkaf</a>
+                    </p>
+                </div>
             </Grid>
         );
     }
