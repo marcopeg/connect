ea5f93d2ab52f6746e45222e7bb9a1bd31d2cf9d
Marco Pegoraro <marco.pegoraro@gmail.com>
2015-09-27 07:29:10 +0200

    home screen style

--- app/containers/StartPage.js
+++ app/containers/StartPage.js
@@ -3,6 +3,8 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 import Grid from 'react-bootstrap/lib/Grid';
+import Well from 'react-bootstrap/lib/Well';
+import Glyphicon from 'react-bootstrap/lib/Glyphicon';
 
 import { StartScreen } from 'components/StartScreen';
 import { changePage } from 'services/active-page-service';
@@ -22,6 +24,23 @@ export class StartPage extends React.Component {
                     onEditProfile={$=> dispatch(changePage('edit'))} 
                     onShowConnections={$=> dispatch(changePage('list'))} 
                     onConnectFacebook={$=> dispatch(connectFacebook())} />
+
+                <hr />
+
+                <p className="lead">
+                    <b>ConnectAPP</b> allows you to quickly bond with other guys
+                    in <b>events as conferences</b> or such where there is no
+                    time to save a phone number or search through Facebook.
+                </p>
+                <Well bsSize="xsmall" className="text-center">
+                    <p className="lead" style={{margin:0}}>
+                        Click<br /> 
+                        <a className="btn btn-success btn-xs" onClick={$=> dispatch(changePage('connect'))}>
+                            <Glyphicon glyph="magnet" /> Connect Now!
+                        </a> 
+                        <br /> to discover people around you. 
+                    </p>
+                </Well>
             </Grid>
         );
     }
